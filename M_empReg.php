7e1dbcb165b4800aa1e0d16fab1d46a6b417115c
thimiramanujaya <128280023+thimiramanujaya@users.noreply.github.com>
2023-10-20 23:56:37 +0530

    restrict registering same value for unique fields
    set alert and redirect to same page when Manager registering same value for unique table fields in Employee & Task tables
    Employee -> Eid(p), name, email
    Task -> Tid(p), Tname

--- M_empReg.php
+++ M_empReg.php
@@ -17,17 +17,28 @@
 	
 	// create connection
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	
-	
-	$sql = "INSERT INTO Employee VALUES ('$eid', '$tele', '$name', '$email', '$design')";
-	
-	if(mysqli_query($conn, $sql)) {
-		$_SESSION['message'] = "New Employee Registered Successfully";
-        header('Location: M_Task.php');								 
+
+
+	$check_primary_key = "SELECT eid FROM Employee WHERE eid='$eid' OR tel='$tele' OR email='$email'";
+	$primary_key_result = mysqli_query($conn, $check_primary_key);
+
+	if(mysqli_num_rows($primary_key_result) > 0) {
+		$_SESSION['message'] = "This employee has been already Registered<br/>Please try again";
+		header('Location: M_Employee.php');
+				
 	}
 	else {
-		echo "Error:".$sql."<br/>".mysqli_error($conn); 
+		$sql = "INSERT INTO Employee VALUES ('$eid', '$tele', '$name', '$email', '$design')";
+	
+		if(mysqli_query($conn, $sql)) {
+			$_SESSION['message'] = "New Employee Registered Successfully";
+        	header('Location: M_Task.php');								 
+		}
+		else {
+			echo "Error:".$sql."<br/>".mysqli_error($conn); 
+		}
 	}
+
 	
 	//close connection
 	mysqli_close($conn);
--- M_regTask.php
+++ M_regTask.php
@@ -17,17 +17,27 @@
 	
 	// create connection
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	
-	
-	$sql = "INSERT INTO Task VALUES ('$tid', '$tname', '$strdate', '$enddate', '$tnature')";
 
-    if(mysqli_query($conn, $sql)) {
-		$_SESSION['message'] = "New Task Registered Successfully";
-        header('Location: M_Activity.php');	
+	$check_primary_key = "SELECT tid FROM Task WHERE tid='$tid' OR tname='$tname'";
+	$primary_key_result = mysqli_query($conn, $check_primary_key);
+
+	if(mysqli_num_rows($primary_key_result) > 0) {
+		$_SESSION['message'] = "This Task has already been Registered<br/>Please try again";
+		header('Location: M_Task.php');
+				
 	}
 	else {
-		echo "Error:".$sql."<br/>".mysqli_error($conn); 
+		$sql = "INSERT INTO Task VALUES ('$tid', '$tname', '$strdate', '$enddate', '$tnature')";
+
+    	if(mysqli_query($conn, $sql)) {
+			$_SESSION['message'] = "New Task Registered Successfully";
+        	header('Location: M_Activity.php');	
+		}
+		else {
+			echo "Error:".$sql."<br/>".mysqli_error($conn); 
+		}
 	}
+
 	
 	//close connection
 	mysqli_close($conn);
