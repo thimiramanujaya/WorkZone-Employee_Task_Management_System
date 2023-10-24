187f7ffc924b4b665661b9fb77ffb73bf54032a4
thimiramanujaya <128280023+thimiramanujaya@users.noreply.github.com>
2023-10-24 07:46:14 +0530

    Implemented table data display in employee report
    Designed to show details according to an employee id when if it is given, if not given, when select generate report button, warning alert will pop up
    
    More - applied some modifications for the style of eid input in report pages

--- E_Report.php
+++ E_Report.php
@@ -1,5 +1,8 @@
 
 <?php
+
+    session_start();
+
 	$hostname = "localhost";
 	$username = "root";
 	$password = "";
@@ -9,19 +12,26 @@
 
     $record = "";
     if(isset($_GET['gen_report'])) {
-        $query = "SELECT eid, name, tname, activity, dateassign 
-                  FROM Employee, Task, taskactivites, assign";
+        $eid = $_GET['emp_id'];
+        if($eid != "") {
+            $query = "SELECT Employee.eid, Employee.name, Employee.designation, Task.tname, Task.start_date, Task.end_date, taskactivites.activity, assign.dateassign, assign.remarks
+            FROM Employee, Task, taskactivites, assign
+            WHERE Employee.eid='$eid' AND Employee.eid = assign.eid AND Task.tid = assign.tid AND taskactivites.activityid = assign.activityid";
 
-		$query_result = mysqli_query($conn, $query);
+            $query_result = mysqli_query($conn, $query);
 
-		if(mysqli_num_rows($query_result) > 0) {
-			while($result = mysqli_fetch_array($query_result)) {
-				$record = $record."<tr> <td>{$result['eid']}</td> <td>{$result['name']}</td> <td>{$result['tname']}</td> 
-                <td>{$result['activity']}</td> <td>{$result['dateassign']}</td> </tr>";
-			}
-		}
+            if(mysqli_num_rows($query_result) > 0) {
+                while($result = mysqli_fetch_array($query_result)) {
+                  $record = $record."<tr> <td>{$result['eid']}</td> <td>{$result['name']}</td> <td>{$result['designation']}</td> <td>{$result['tname']}</td> 
+                  <td>{$result['start_date']}</td> <td>{$result['end_date']}</td> <td>{$result['activity']}</td> <td>{$result['dateassign']}</td> <td>{$result['remarks']}</td> </tr>";
+                }
+              }
+            else {
+                $_SESSION['message'] = "Error: No sufficient data to create Record";
+            }
+        }
         else {
-            echo '<script>alert("Error: No Sufficient data to create Record")</script>';
+            $_SESSION['message'] = "Please give a valid Employee Id";
         }
     }
 
@@ -36,22 +46,48 @@
         <link text="stylesheet" rel="stylesheet" href="form-style.css">
     </head>
 
+    <script>
+        function closeAlert() {
+            document.getElementById('alert').style.display = 'none';
+        }
+    </script>
+
     <body>
         <div class="e_form_container">
+        <?php 
+            if(isset($_SESSION['message'])) {
+        ?>
+                 <div class="index_alert_box" id="alert">
+                    <h4><?= $_SESSION['message']; ?></h4>
+                    <button type="button"><img src="res/close_icon.png" width="20px" height="20px" onclick="closeAlert()"/></button>
+                 </div>
+
+        <?php
+                unset($_SESSION['message']);
+            }
+        ?>
         <a href="Employee_Dashboard.php"><img src="res/back_arrow.png" width="32px" height="32px"/> Back to Dashboard</a>
             <div class="report_container">
                 <h1>Assigned Activities</h1>
                 <form method="get">
-                    <input type="submit" value="View Assigned Activities" name="gen_report" class="button_box" />
+                    <div class="eid_box input_box">
+                        <label for="eid">Your Employee id</label>
+                        <input type="text" id="E_eid" name="emp_id"/>
+                    </div>
+                    <input type="submit" value="View Assigned Activities" name="gen_report" class="button_box" id="E_gen_report"/>
                 </form>
 
                 <table>
                     <tr>
                         <th>Employee Id</th>
                         <th>Employee Name</th>
+                        <th>Employee Designation</th>
                         <th>Task Assigned</th>
+                        <th>Task Start Date</th>
+                        <th>Task End Date</th>
                         <th>Activity</th>
                         <th>Date Assigned</th>
+                        <th>Remarks</th>
                     </tr>
                     <?php echo $record; ?>
                 </table>
